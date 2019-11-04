ed574d4d51783e009119f26c3ef2b34fe4c95bd0
dlama187 <56181256+dlama187@users.noreply.github.com>
2019-11-03 22:33:23 -0600

    Delete mod3_opendb.php

--- Module_03/project/mod3_opendb.php
+++
@@ -1,8 +0,0 @@
-<?php
-//create connection
-$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
-//check connection
-if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
-}
-?>
